77dd97dd83d36ea7e56e409ffb1f2aab09ea4f3a
Matt Peng <mattukepeng@gmail.com>
2023-06-26 14:02:59 -0500

    pi

--- commands/pi.js
+++ commands/pi.js
@@ -1,7 +1,7 @@
 const { respond_interaction } = require("../discord_request");
 const Big = require('big.js');
 
-function falling_factorial(n, k) {
+async function falling_factorial(n, k) {
 	let result = new Big(1);
 	for (let i = new Big(0); i < k; i++) {
 		result = result.times(n - i);
@@ -9,7 +9,7 @@ function falling_factorial(n, k) {
 	return result;
 }
 
-function factorial(n) {
+async function factorial(n) {
 	let result = new Big(1);
 	for (let i = new Big(0); i < n; i++) {
 		result = result.times(n - i);
@@ -17,11 +17,11 @@ function factorial(n) {
 	return result;
 }
 
-function coefficient(n, k) {
+async function coefficient(n, k) {
 	return falling_factorial(n, k).div(factorial(k));
 }
 
-function nth_term(n, r) {
+async function nth_term(n, r) {
 	let k = new Big(-1);
 	let x = new Big(r.pow(2 * n + 1))
 	k = k.pow(n)
@@ -31,21 +31,23 @@ function nth_term(n, r) {
 	return k;
 }
 
-function pi_calc(n) {
+async function pi_calc(n) {
 	let result = new Big(0)
 	let three = new Big(3)
 	let eight = new Big(8)
 	let r = new Big(0.5)
 	for (let i = 0; i < n; i++){
-		result = result.add(nth_term(i, r))
+		result = result.add(await nth_term(i, r))
 	}
 	result -= three.sqrt().div(eight)
+
 	return result*12
 }
 
 async function pi(interaction) {
 	let n = interaction.data.options[0].value
 	let piCalculated = pi_calc(n)
+	console.log(piCalculated)
 	respond_interaction(interaction, {
 		"embeds": [
 			{
